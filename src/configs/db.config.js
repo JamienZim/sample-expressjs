427378531e35b86e8193ba32ce76998feeff4ad7
JamienZim <95596876+JamienZim@users.noreply.github.com>
2024-04-28 10:45:56 +1000

    adding .env file

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,28 +1,17 @@
 // const env = process.env;
 const fs = require('fs');
+require('dotenv').config()
 
 // ssl: {
 //     mode: 'VERIFY_IDENTITY',
 //     ca: fs.readFileSync('/etc/ssl/cert.pem', 'utf-8'),
 //   }
 
-const dbconfig = {
-    username: "doadmin",
-    password: "AVNS_CSj4CfCksFUPPOG3kHg",
-    host: "db-mysql-syd1-26793-do-user-16478014-0.c.db.ondigitalocean.com",
-    port: "25060",
-    database: "defaultdb",
-    sslmode: "REQUIRED"
-}
-
-const env = process.env;
 const db = {
-    host: dbconfig.host,
-    user: dbconfig.username,
-    password: dbconfig.password,
-    database: dbconfig.database,
-    port: dbconfig.port,
-
+    host: process.env.defaultdbHost,
+    user: process.env.defaultdbUN,
+    password: process.env.defauldbPW,
+    database: "defaultdb",
+    port: process.env.defaultdbPort
 };
-
 module.exports = db;
