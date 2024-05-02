5c13cb849beb687d5702382b83f03d0c5afebde0
JamienZim <95596876+JamienZim@users.noreply.github.com>
2024-05-02 12:15:46 +1000

    Update rnsw.db.service.js

--- src/services/rnsw.db.service.js
+++ src/services/rnsw.db.service.js
@@ -18,14 +18,15 @@ async function insertOne(collection, doc) {
     }
 }
 
-async function queryFind(collection, query, projection, options) {
+async function queryFind(collection, query, projection={}, options) {
     try{
         await client.connect();
         const database = client.db(dbconfig.db.RNSWDB);
         const col = database.collection(collection);
-        const response = await col.find(query, projection, options).toArray(function(err, result) {
+        console.log(`projection: ${JSON.stringify(projection)} `);
+        const response = await col.find(query).project(projection).toArray(function(err, result) {
             if (err) throw err;
-            console.log(result);
+            // console.log(result);
             db.close();
           });
         console.log(response);
@@ -47,7 +48,16 @@ async function loadAcceptances(acceptances){
 
 async function getAcceptance(meetcode){
     try {
-        const response = await queryFind('acceptances', {'Meeting.@MeetCode': meetcode}, {}, {});
+        const response = await queryFind('acceptances', 
+        {'Meeting.@MeetCode': meetcode}, 
+{"Meeting.!": 0, "Meeting.@InputFilename": 0, 
+"Meeting.Races.Race.TrackRecords" : 0, 
+"Meeting.Races.Race.RaceEntries.RaceEntry.Form.FormSummary" : 0, 
+"Meeting.Races.Race.RaceEntries.RaceEntry.Form.ResultsSummaries" : 0, 
+"Meeting.Races.Race.RaceEntries.RaceEntry.Form.LastStarts" : 0,
+"Meeting.Races.Race.RaceEntries.RaceEntry.HorseOwnership": 0,
+"Meeting.Races.Race.RaceEntries.RaceEntry.Breeding": 0}, 
+        {});
         return response;
     } catch (error) {
         console.log(error);
